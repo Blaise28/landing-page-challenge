a64f48f2767c698744e945d6a4b09ea05015f15f
Blaise28 <poseudo33@gmail.com>
2023-05-29 10:21:05 +0200

    navbar

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const mobileNavBar = document.getElementById("offcanvasNavbar");
 const humbergerButton = document.querySelector(".navbar-toggler");
 const closeButton = document.querySelector(".offcanvas-header .btn");
 
-humbergerButton.addEventListener('click',()=>{
-    humbergerButton.classList.add('d-none');
-})
+humbergerButton.addEventListener("click", () => {
+  const overlay = document.querySelector(".offcanvas-backdrop");
+  overlay.addEventListener("click", () => {
+    humbergerButton.classList.remove("d-none");
+  });
+  humbergerButton.classList.add("d-none");
+});
 
-closeButton.addEventListener("click",()=>{
-  humbergerButton.classList.remove('d-none');
-})
-
+closeButton.addEventListener("click", () => {
+  humbergerButton.classList.remove("d-none");
+});
